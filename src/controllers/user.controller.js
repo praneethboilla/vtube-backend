3a647672c51556dfec35ee097f0dcad10550c81b
Praneeth <boillapraneeth10@gmail.com>
2024-12-29 16:23:36 +0530

    login, logout routes and auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,27 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler( async (req, res) => {
+ // generate tokens using methods created in user.model like generateAccessToken, generateRefreshToken
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong in generating tokens")
+    }
+}
+
+const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     // validation
     // check if user already exists
@@ -15,16 +32,19 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return response 
     const { fullName, email, username, password } = req.body
-
+  
+    //check if field are empty using some() returns boolean
+    //can add email format validation using regex
     if ([fullName, email, username, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser =  await User.findOne({
+    // check existing user using anyone of username, email using mongodb $or
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
-    if(existedUser){
+    if (existedUser) {
         throw new ApiError(409, "User with email already exists")
     }
 
@@ -32,7 +52,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path;
     }
 
@@ -40,10 +60,11 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "avatar file is required");
     }
 
+    // upload on cloudinary using local paths created by multer stored in upload folder
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar) {
+    if (!avatar) {
         throw new ApiError(400, "avatar file is required");
     }
 
@@ -57,9 +78,9 @@ const registerUser = asyncHandler( async (req, res) => {
     });
 
     const createdUser = await User.findById(user._id)
-    .select("-password -refreshToken")
+        .select("-password -refreshToken")
 
-    if(!createdUser){
+    if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering user")
     }
 
@@ -69,5 +90,79 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
+const loginUser = asyncHandler(async (req, res) => {
+    // get details from frontend
+    // validate username or email are given
+    // find user
+    // user password check
+    // generate access and refresh token
+    // send cookies 
+    const { username, email, password } = req.body
 
-export { registerUser }
+    if (!username || !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+    // login using anyone of username, email using mongodb $or
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does nor exist")
+    }
+    // check password from db using isPasswordCorrect method created in user.model
+    const isPasswordValid = await User.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "invalid user credentials")
+    }
+
+    // generate tokens using methods created in user.model
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id)
+        .select("-password -refreshToken")
+
+    // send tokens to frontend by using cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            // optional to send tokens already sent in cookies // sent because if user want to store in localStorage
+            new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken }, "User logged in Successfully")
+        )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    // remove refreshToken from db 
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    // clear tokens from cookies from client
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"))
+
+})
+
+
+export { registerUser, loginUser, logoutUser }
