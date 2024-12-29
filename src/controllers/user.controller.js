03983b7c72b1c5ed0ca805652b25f21c846a28c9
Praneeth <boillapraneeth10@gmail.com>
2024-12-29 14:35:58 +0530

    register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser =  await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -29,7 +29,12 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "avatar file is required");
